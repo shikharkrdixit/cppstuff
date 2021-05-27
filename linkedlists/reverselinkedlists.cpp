00075dbba9426df3ded3e98e9ce7e12e7fab71a7
Shikhar Dixit <sdixit362@gmail.com>
2021-05-27 23:33:28 +0530

    reverselinkedlists

--- linkedlists/reverselinkedlists.cpp
+++ linkedlists/reverselinkedlists.cpp
@@ -1,4 +1,4 @@
-// P2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// reverselinkedlists.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <bits/stdc++.h>
